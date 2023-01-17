9ee560cc863334bf04980953c3b1c390320524ab
Lubomir Dolezal <lubomir.dolezal@eox.at>
2023-01-17 19:32:44 +0100

    implemented versatile check of letter

--- test/test_homework_1.py
+++ test/test_homework_1.py
@@ -3,19 +3,19 @@ from lesson13.pets_letter import pets_begin_d
 
 def test_hw1_empty():
     input_f = []
-    result = pets_begin_d(input_f)
+    result = pets_begin_d(input_f, "d")
     assert result == []
 
 
 def test_hw1_contains_d():
     input_f = ["d", "dd", "tiger"]
-    result = pets_begin_d(input_f)
+    result = pets_begin_d(input_f, "d")
     assert result == ["d", "dd"]
 
 
 def test_hw1_standard_input():
     input_f = ["Dog", "cat", "dolphin", "rabbit", "snake"]
-    result = pets_begin_d(input_f)
+    result = pets_begin_d(input_f, "d")
     assert result == ["dolphin"]
 
 
